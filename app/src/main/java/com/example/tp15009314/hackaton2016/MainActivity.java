c38a0ff6a5fbfc76b601913a1df25b086a95fff7
Grégory <greg.pennetier@gmail.com>
2016-11-29 11:14:04 +0100

    connexion

--- app/src/main/java/com/example/tp15009314/hackaton2016/MainActivity.java
+++ app/src/main/java/com/example/tp15009314/hackaton2016/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.tp15009314.hackaton2016;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("");
+
+        System.out.println(myRef.child("5147"));
     }
 
     public void AfficherListe(View view) {
