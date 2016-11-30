035de7bb464598325f1b84ad5ff29ad2ed83dfd6
Grégory <greg.pennetier@gmail.com>
2016-11-30 13:53:17 +0100

    maps

--- app/src/main/java/com/example/tp15009314/hackaton2016/MapsActivity.java
+++ app/src/main/java/com/example/tp15009314/hackaton2016/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -48,6 +49,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        UiSettings settings = mMap.getUiSettings();
+        settings.setCompassEnabled(true);
+        settings.setMapToolbarEnabled(true);
+        settings.setMyLocationButtonEnabled(true);
         // Add a marker in Sydney and move the camera
         LatLng point = new LatLng(lat, lon);
         mMap.addMarker(new MarkerOptions().position(point).title(nom));
